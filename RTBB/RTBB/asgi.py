c0cd974d587fbe42bb00e1852ccadeaaae083840
Kalvezu <36405170+Kalvezu@users.noreply.github.com>
2023-05-25 08:15:35 +0200

    Project
    Quatsch

--- RTBB/RTBB/asgi.py
+++ RTBB/RTBB/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RTBB project.
+ASGI config for rtbb project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RTBB.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rtbb.settings')
 
 application = get_asgi_application()
